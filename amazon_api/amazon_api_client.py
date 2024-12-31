614417b97085ea30802bd2b88b5f273f66d7e7c7
Pamela Vermeer <vermeerp@gmail.com>
2024-12-30 20:51:44 -0600

    add inventory

--- amazon_api/amazon_api_client.py
+++ amazon_api/amazon_api_client.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta
-from sp_api.api import Orders
+from sp_api.api import Orders, Inventories
 from sp_api.util import throttle_retry, load_all_pages
 
 
@@ -10,4 +10,6 @@ def load_all_orders(**kwargs):
     a generator function to return all pages, obtained by NextToken
     """
     return Orders().get_orders(**kwargs)
-
+()
+def get_inventory(**kwargs):
+    return Inventories().get_inventory_summary_marketplace(**kwargs)
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # run_amazon_api_client.py
-from amazon_api.amazon_api_client import load_all_orders
+from amazon_api.amazon_api_client import load_all_orders, get_inventory
 from datetime import datetime, timedelta
 
 def get_orders(LastUpdatedAfter: datetime):
@@ -9,4 +9,9 @@ def get_orders(LastUpdatedAfter: datetime):
 
 if __name__ == "__main__":
     LastUpdatedAfter = (datetime.now() - timedelta(days=2)).isoformat()
-    get_orders(LastUpdatedAfter)
+    # get_orders(LastUpdatedAfter)
+
+    print(get_inventory(**{
+        "details": True,
+        "marketplaceIds": ["A2Q3Y263D00KWC"]
+    }))
